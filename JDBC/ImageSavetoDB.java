e75478dae41d9b83adf488041eb7e19238a65d2b
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-06-23 23:59:13 +0530

    Modify

--- JDBC/ImageSavetoDB.java
+++ JDBC/ImageSavetoDB.java
@@ -5,7 +5,7 @@ public class ImageSavetoDB {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Ujjawaldatabase", "root", "root123");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "root", "root123");
             // create table iamges(id int primary key auto_increment, pic blog);
             // blog -- binary large object
             
